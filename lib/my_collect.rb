4b10fbfa0125ff7004d4ac39b00344fdf56f2d59
Eoghan Leddy <eoghan.leddy@flatironschool.com>
2016-09-23 21:34:46 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,17 @@
-
-
+array_1 = ["john walken", "jameson crawlen", "andson sleepin"]
+students = ['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller']
+def my_collect(array)
+  if block_given?
+    new_arr = []
+    new_item = nil
+    counter = 0
+    while counter < array.length
+      new_item = yield array[counter]
+      new_arr << new_item
+      counter += 1
+    end
+  else
+    print "No block was given meng"
+  end
+  return new_arr
+end
